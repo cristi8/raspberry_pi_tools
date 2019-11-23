4040d2c9d49361e0e6454f3fd9f8aa70b71c2f29
= <cristi8@gmail.com>
2019-11-24 01:42:27 +0200

    rpi_exporter

---
+++ rpi_exporter/rpi_exporter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import prometheus_client
+import os
+import time
+import logging
+import socket
+
+logger = logging.getLogger(__name__)
+HOSTNAME = socket.gethostname()
+
+MON_TEMP = prometheus_client.Gauge('temp', 'Temperature', ['hostname'])
+
+
+def measure_temp():
+    output = os.popen("vcgencmd measure_temp").readline().strip()
+    return float(output[5:-2])
+
+
+def update_stats():
+    MON_TEMP.labels(HOSTNAME).set(measure_temp())
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    prometheus_client.start_http_server(9101)
+
+    while True:
+        try:
+            update_stats()
+        except Exception as ex:
+            logger.error("Error updating stats: %s", str(ex))
+        time.sleep(5)
+
+
+if __name__ == '__main__':
+    main()
